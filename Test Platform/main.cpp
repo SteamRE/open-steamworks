0cd6fb69129cc737e3803f517acf2d383acb95e7
Ryan Stecker <voidedweasel@gmail.com>
2009-12-23 04:12:54 +0000

    Added test code for steam2/steam3 login. No dice on steam3 login.. FML.
    --HG--
    extra : convert_revision : svn%3Af45044ad-9681-42ff-a337-a86ec09c9930%4063

--- Test Platform/main.cpp
+++ Test Platform/main.cpp
@@ -1,84 +1,137 @@
-
-#include <windows.h>
-
-#include <string>
-#include <cstdio>
-#include <iostream>
-#include <iomanip>
-
 #include "Steamworks.h"
+
+#pragma comment(lib, "../steam")
+#pragma comment(lib, "../steamclient")
+
+using std::string;
+using std::cout;
+using std::cin;
+
+#define STEAM_ERROR(err) ( err.eSteamError != eSteamErrorNone )
 
 int main()
 {
+	// load steamclient
+	CreateInterfaceFn interfaceFn = Sys_GetFactory( "steamclient.dll" );
 
-	SetEnvironmentVariable( "SteamAppId", "440" );
-
-	CSteamAPILoader loader;
-
-	CreateInterfaceFn factory = loader.Load();
-	if ( !factory )
+	IClientEngine *clientEngine = (IClientEngine *)interfaceFn( CLIENTENGINE_INTERFACE_VERSION, NULL );
+	if ( !clientEngine )
 	{
-		std::cout << "unable to load steamclient.\n";
-		getchar();
-		return 0;
+		cout << "Unable to get IClientEngine.\n";
+		return 1;
 	}
 
-	ISteamClient008 *steamClient = (ISteamClient008 *)factory( STEAMCLIENT_INTERFACE_VERSION_008, NULL );
-	if ( !steamClient )
+	HSteamPipe hPipe;
+	HSteamUser hUser = clientEngine->CreateGlobalUser( &hPipe );
+
+	if ( !hUser || !hPipe )
 	{
-		std::cout << "no interface.\n";
-		getchar();
-		return 0;
+		cout << "Unable to create global user.\n";
+		return 1;
 	}
 
-	HSteamPipe pipe = steamClient->CreateSteamPipe();
-	HSteamUser user = steamClient->ConnectToGlobalUser( pipe );
-
-	ISteamGameCoordinator001 *coord = (ISteamGameCoordinator001 *)steamClient->GetISteamGenericInterface( user, pipe, STEAMGAMECOORDINATOR_INTERFACE_VERSION_001 );
-
-	CallbackMsg_t callbackMsg;
-	HSteamCall steamCall;
-
-	coord->SendMessage( 1002, "test", 4 );
-
-	while ( true )
+	TSteamError error;
+	
+	SteamClearError( &error );
+	if ( !SteamStartEngine( &error ) )
 	{
-		uint32 msg;
-		if ( coord->IsMessageAvailable( &msg ) )
-		{
-			std::cout << "got message: " << msg << "\n";
-		}
-
-
-
-		if ( Steam_BGetCallback( pipe, &callbackMsg, &steamCall ) )
-		{
-
-
-			int32 callBack = callbackMsg.m_iCallback;
-			ECallbackType type = (ECallbackType)((callBack / 100) * 100);
-
-			std::cout << "[SERVER] Unhandled Callback: " << callBack << ", Type: " << EnumString<ECallbackType>::From(type) << ", Size: " << callbackMsg.m_cubParam << std::endl;
-
-			int32 callSize = callbackMsg.m_cubParam;
-			unsigned char *data = callbackMsg.m_pubParam;
-			std::cout << "  ";
-			for (int i = 0; i < callSize; i++)
-			{
-				unsigned char value = data[i];
-
-				std::cout << std::hex << std::setw(2) << std::setfill('0') << std::uppercase << (unsigned int)value;
-				std::cout << " ";
-
-			}
-
-			std::cout << std::resetiosflags(std::ios_base::hex | std::ios_base::uppercase) << std::endl;
-
-			Steam_FreeLastCallback( pipe );
-
-		}
-		Sleep(10);
+		cout << "Unable to start steam engine: " << error.szDesc << "\n";
+		return 1;
 	}
+
+	SteamClearError( &error );
+	if ( !SteamStartup( STEAM_USING_ALL, &error ) )
+	{
+		cout << "Unable to startup steam: " << error.szDesc << "\n";
+		return 1;
+	}
+
+	FactoryFn _f = Sys_GetFactoryOld( "Steam.dll" );
+	if ( !_f )
+	{
+		cout << "Unable to get steam factory.\n";
+		return 1;
+	}
+
+	ISteam006 *steam = (ISteam006 *)_f( STEAM_INTERFACE_VERSION_006 );
+	if ( !steam )
+	{
+		cout << "Unable to get Steam interface.\n";
+		return 1;
+	}
+
+	string strUser;
+	string strPass;
+
+	cout << "Username: ";
+	cin >> strUser;
+
+	cout << "Password: ";
+	cin >> strPass;
+
+	SteamCallHandle_t loginHandle = steam->Login( strUser.c_str(), strPass.c_str(), true, &error );
+	if ( loginHandle == STEAM_INVALID_CALL_HANDLE )
+	{
+		cout << "Invalid login handle.\n";
+		return 1;
+	}
+
+	steam->BlockingCall( loginHandle, 100, &error );
+	if ( STEAM_ERROR( error ) )
+	{
+		cout << "Unable to login: " << error.szDesc << "\n";
+		return 1;
+	}
+
+	char szUser[ MAX_PATH ];
+	uint32 nameLen;
+	size_t len = strUser.copy( szUser, MAX_PATH );
+	szUser[ len ] = 0;
+
+	_strlwr( (char*)&szUser );
+
+	SetEnvironmentVariable( "SteamUser", szUser );
+	SetEnvironmentVariable( "SteamAppUser", szUser );
+
+	TSteamGlobalUserID userId;
+
+	IClientUtils *utils = clientEngine->GetIClientUtils( hPipe, CLIENTUTILS_INTERFACE_VERSION );
+	IClientUser *user = clientEngine->GetIClientUser( hUser, hPipe, CLIENTUSER_INTERFACE_VERSION );
+	if ( !utils || !user )
+	{
+		cout << "Unable to get IClientUtils or IClientUser.\n";
+		return 1;
+	}
+
+	steam->GetUser( szUser, MAX_PATH, &nameLen, &userId, &error );
+	if ( STEAM_ERROR( error ) )
+	{
+		cout << "Unable to GetUser: " << error.szDesc << "\n";
+		return 1;
+	}
+
+	EUniverse universe = utils->GetConnectedUniverse();
+	CSteamID steamId;
+
+	steamId.SetFromSteam2( &userId, universe );
+
+	
+
+	user->LogOn( steamId );
+	ELogonState state = user->GetLogonState();
+	while ( state == k_ELogonStateLoggingOn )
+	{
+		Sleep( 10 );
+		state = user->GetLogonState();
+	}
+
+	if ( state != k_ELogonStateLoggedOn )
+	{
+		cout << "Logon failed.";
+		return 1;
+	}
+
 
 	return 0;
+
 }
