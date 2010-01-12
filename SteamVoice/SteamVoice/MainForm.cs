30e314ca323e74b3d1a4a5c952aff3a4e6af5272
Ryan Stecker <voidedweasel@gmail.com>
2010-01-12 01:55:15 +0000

    Added SteamVoice, a VoIP project that died before it even started.
    --HG--
    extra : convert_revision : svn%3Af45044ad-9681-42ff-a337-a86ec09c9930%4089

---
+++ SteamVoice/SteamVoice/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using VoiceSupport;
+
+namespace SteamVoice
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            Steam.Init();
+            PlayBack.Init();
+        }
+
+        private void button1_Click( object sender, EventArgs e )
+        {
+            Steam.StartRecording();
+        }
+
+        private void button2_Click( object sender, EventArgs e )
+        {
+            Steam.StopRecording();
+
+            byte[] data = Steam.GetCompressedVoiceData();
+
+            byte[] decomp = Steam.DecompressVoice( data );
+
+            PlayBack.PlayData( decomp );
+        }
+    }
+}
---
+++ SteamVoice/VoiceSupport/Steam.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using dotnetworks;
+
+namespace VoiceSupport
+{
+    public class Steam
+    {
+        static SteamClient008 steamClient;
+
+        static SteamPipeHandle pipe;
+        static SteamUserHandle user;
+
+        static SteamUser012 steamUser;
+
+        public static void Init()
+        {
+            int error;
+            steamClient = (SteamClient008)Steamworks.CreateInterface( SteamClient008.InterfaceVersion, out error );
+
+            pipe = steamClient.CreateSteamPipe();
+            user = steamClient.ConnectToGlobalUser( pipe );
+
+            steamUser = (SteamUser012)steamClient.GetISteamUser( user, pipe, SteamUser012.InterfaceVersion );
+        }
+
+        public static void StartRecording()
+        {
+            steamUser.StartVoiceRecording();
+        }
+
+        public static void StopRecording()
+        {
+            steamUser.StopVoiceRecording();
+        }
+
+        public static byte[] GetCompressedVoiceData()
+        {
+            uint buffSize = 1024 * 20; // start with 20kb
+            byte[] voiceBuff = new byte[ buffSize ];
+            uint written;
+            VoiceResult result;
+
+            while ( true )
+            {
+                result = steamUser.GetCompressedVoice( voiceBuff, buffSize, out written );
+
+                if ( result == VoiceResult.DataCorrupted || result == VoiceResult.NoData || result == VoiceResult.NotInitialized )
+                    return new byte[ 0 ];
+
+                if ( result == VoiceResult.OK || result == VoiceResult.NotRecording )
+                    break;
+
+                if ( result == VoiceResult.BufferTooSmall )
+                {
+                    buffSize += ( 1024 * 10 ); // add another 10kb
+                    voiceBuff = new byte[ buffSize ];
+                    continue;
+                }
+            }
+
+            byte[] returnData = new byte[ written ];
+            Array.Copy( voiceBuff, 0, returnData, 0, written );
+
+            return returnData;
+        }
+
+        public static byte[] DecompressVoice( byte[] input )
+        {
+            uint decompSize = (uint)( input.Length * 2 );
+            byte[] decompressed = new byte[ decompSize ];
+            VoiceResult result;
+            uint written;
+
+            while ( true )
+            {
+                result = steamUser.DecompressVoice( input, (uint)input.Length, decompressed, decompSize, out written );
+
+                if ( result == VoiceResult.DataCorrupted || result == VoiceResult.NoData || result == VoiceResult.NotInitialized )
+                    return new byte[ 0 ];
+
+                if ( result == VoiceResult.OK )
+                    break;
+
+                if ( result == VoiceResult.BufferTooSmall )
+                {
+                    decompSize += ( 1024 * 10 );
+                    decompressed = new byte[ decompSize ];
+                    continue;
+                }
+            }
+
+            byte[] returnData = new byte[ written ];
+            Array.Copy( decompressed, 0, returnData, 0, written );
+
+            return returnData;
+        }
+    }
+}
