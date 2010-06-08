7b2b339c54edb6597f1f2de191336813b2189943
Asher Baker <asherkin@limetech.org>
2010-06-08 10:57:01 +0000

    Fixing SteamBGetCallbackFn.
    --HG--
    extra : convert_revision : svn%3Af45044ad-9681-42ff-a337-a86ec09c9930%40197

--- Open Steamworks/SteamTypes.h
+++ Open Steamworks/SteamTypes.h
@@ -153,7 +153,7 @@ typedef void (*KeyValueIteratorCallback_t)(const char* key, const char* value, v
 
 typedef void (*SteamNotificationCallback_t)(ESteamNotify eEvent, unsigned int nData);
 
-typedef bool (*SteamBGetCallbackFn)( int hpipe, void *pCallbackMsg, int32 *phSteamCall );
+typedef bool (*SteamBGetCallbackFn)( int hpipe, void *pCallbackMsg );
 typedef void (*SteamFreeLastCallbackFn)( int hpipe );
 typedef bool (*SteamGetAPICallResultFn)( int hpipe, uint64 hSteamAPICall, void* pCallback, int cubCallback, int iCallbackExpected, bool* pbFailed );
 
