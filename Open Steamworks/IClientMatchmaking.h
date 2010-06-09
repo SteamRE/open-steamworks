70569364a1d564d0c5d880547e394c8a309d7c49
Ryan Stecker <voidedweasel@gmail.com>
2010-06-09 05:55:04 +0000

    Updated IClientMatchmaking.
    --HG--
    extra : convert_revision : svn%3Af45044ad-9681-42ff-a337-a86ec09c9930%40202

--- Open Steamworks/IClientMatchmaking.h
+++ Open Steamworks/IClientMatchmaking.h
@@ -28,45 +28,61 @@
 class UNSAFE_INTERFACE IClientMatchmaking
 {
 public:
-	virtual unknown_ret GetFavoriteGameCount( void ) = 0;
-	virtual unknown_ret GetFavoriteGame( int, unsigned int *, unsigned int *, unsigned short *, unsigned short *, unsigned int *, unsigned int * ) = 0;
-	virtual unknown_ret AddFavoriteGame( unsigned int, unsigned int, unsigned short, unsigned short, unsigned int, unsigned int ) = 0;
-	virtual unknown_ret RemoveFavoriteGame( unsigned int, unsigned int, unsigned short, unsigned short, unsigned int ) = 0;
-	virtual unknown_ret RequestLobbyList( void ) = 0;
-	virtual unknown_ret AddRequestLobbyListStringFilter( char  const*, char  const*, ELobbyComparison ) = 0;
-	virtual unknown_ret AddRequestLobbyListNumericalFilter( char  const*, int, ELobbyComparison ) = 0;
-	virtual unknown_ret AddRequestLobbyListNearValueFilter( char  const*, int ) = 0;
-	virtual unknown_ret AddRequestLobbyListFilterSlotsAvailable( int ) = 0;
-	virtual unknown_ret GetLobbyByIndex( int ) = 0;
-	virtual unknown_ret CreateLobby( ELobbyType, int ) = 0;
-	virtual unknown_ret JoinLobby( CSteamID ) = 0;
-	virtual unknown_ret LeaveLobby( CSteamID ) = 0;
-	virtual unknown_ret InviteUserToLobby( CSteamID, CSteamID ) = 0;
-	virtual unknown_ret GetNumLobbyMembers( CSteamID ) = 0;
-	virtual unknown_ret GetLobbyMemberByIndex( CSteamID, int ) = 0;
-	virtual unknown_ret GetLobbyData( CSteamID, char  const* ) = 0;
-	virtual unknown_ret SetLobbyData( CSteamID, char  const*, char  const* ) = 0;
-	virtual unknown_ret GetLobbyDataCount( CSteamID ) = 0;
-	virtual unknown_ret GetLobbyDataByIndex( CSteamID, int, char *, int, char *, int ) = 0;
-	virtual unknown_ret DeleteLobbyData( CSteamID, char  const* ) = 0;
-	virtual unknown_ret GetLobbyMemberData( CSteamID, CSteamID, char  const* ) = 0;
-	virtual unknown_ret SetLobbyMemberData( CSteamID, char  const*, char  const* ) = 0;
-	virtual unknown_ret SendLobbyChatMsg( CSteamID, void  const*, int ) = 0;
-	virtual unknown_ret GetLobbyChatEntry( CSteamID, int, CSteamID*, void *, int, EChatEntryType * ) = 0;
-	virtual unknown_ret RequestLobbyData( CSteamID ) = 0;
-	virtual unknown_ret SetLobbyGameServer( CSteamID, unsigned int, unsigned short, CSteamID ) = 0;
-	virtual unknown_ret GetLobbyGameServer( CSteamID, unsigned int *, unsigned short *, CSteamID* ) = 0;
-	virtual unknown_ret SetLobbyMemberLimit( CSteamID, int ) = 0;
-	virtual unknown_ret GetLobbyMemberLimit( CSteamID ) = 0;
-	virtual unknown_ret SetLobbyVoiceEnabled( CSteamID, bool ) = 0;
-	virtual unknown_ret RequestFriendsLobbies( void ) = 0;
-	virtual unknown_ret SetLobbyType( CSteamID, ELobbyType ) = 0;
-	virtual unknown_ret SetLobbyJoinable( CSteamID, bool ) = 0;
-	virtual unknown_ret GetLobbyOwner( CSteamID ) = 0;
-	virtual unknown_ret SetLobbyOwner( CSteamID, CSteamID ) = 0;
+	virtual int GetFavoriteGameCount() = 0;
+	virtual bool GetFavoriteGame( int iGame, AppId_t *pnAppID, uint32 *pnIP, uint16 *pnConnPort, uint16 *pnQueryPort, uint32 *punFlags, uint32 *pRTime32LastPlayedOnServer ) = 0;
+	virtual int AddFavoriteGame( AppId_t nAppID, uint32 nIP, uint16 nConnPort, uint16 nQueryPort, uint32 unFlags, uint32 rTime32LastPlayedOnServer ) =0;
+	virtual bool RemoveFavoriteGame( AppId_t nAppID, uint32 nIP, uint16 nConnPort, uint16 nQueryPort, uint32 unFlags ) = 0;
 
-	virtual unknown_ret GetGMSServerCount() = 0;
-	virtual unknown_ret GetGMSServerAddress( int a, uint32 *b, uint16 *c ) = 0;
+	virtual SteamAPICall_t RequestLobbyList() = 0;
+
+	virtual void AddRequestLobbyListStringFilter( const char *pchKeyToMatch, const char *pchValueToMatch, ELobbyComparison eComparisonType ) = 0;
+	virtual void AddRequestLobbyListNumericalFilter( const char *pchKeyToMatch, int nValueToMatch, ELobbyComparison eComparisonType ) = 0;
+	virtual void AddRequestLobbyListNearValueFilter( const char *pchKeyToMatch, int nValueToBeCloseTo ) = 0;
+	virtual void AddRequestLobbyListFilterSlotsAvailable( int nSlotsAvailable ) = 0;
+	virtual void AddRequestLobbyListDistanceFilter( ELobbyDistanceFilter filter ) = 0;
+	virtual void AddRequestLobbyListResultCountFilter( int cMaxResults ) = 0;
+
+	virtual CSteamID GetLobbyByIndex( int iLobby ) = 0;
+
+	virtual SteamAPICall_t CreateLobby( ELobbyType eLobbyType, int cMaxMembers ) = 0;
+	virtual SteamAPICall_t JoinLobby( CSteamID steamIDLobby ) = 0;
+	virtual void LeaveLobby( CSteamID steamIDLobby ) = 0;
+	virtual bool InviteUserToLobby( CSteamID steamIDLobby, CSteamID steamIDInvitee ) = 0;
+
+	virtual int GetNumLobbyMembers( CSteamID steamIDLobby ) = 0;
+	virtual CSteamID GetLobbyMemberByIndex( CSteamID steamIDLobby, int iMember ) = 0;
+
+	virtual const char *GetLobbyData( CSteamID steamIDLobby, const char *pchKey ) = 0;
+	virtual bool SetLobbyData( CSteamID steamIDLobby, const char *pchKey, const char *pchValue ) = 0;
+
+	virtual int GetLobbyDataCount( CSteamID steamIDLobby ) = 0;
+	virtual bool GetLobbyDataByIndex( CSteamID steamIDLobby, int iLobbyData, char *pchKey, int cchKeyBufferSize, char *pchValue, int cchValueBufferSize ) = 0;
+	virtual bool DeleteLobbyData( CSteamID steamIDLobby, const char *pchKey ) = 0;
+
+	virtual const char *GetLobbyMemberData( CSteamID steamIDLobby, CSteamID steamIDUser, const char *pchKey ) = 0;
+	virtual void SetLobbyMemberData( CSteamID steamIDLobby, const char *pchKey, const char *pchValue ) = 0;
+
+	virtual bool SendLobbyChatMsg( CSteamID steamIDLobby, const void *pvMsgBody, int cubMsgBody ) = 0;
+	virtual int GetLobbyChatEntry( CSteamID steamIDLobby, int iChatID, CSteamID *pSteamIDUser, void *pvData, int cubData, EChatEntryType *peChatEntryType ) = 0;
+
+	virtual bool RequestLobbyData( CSteamID steamIDLobby ) = 0;
+
+	virtual void SetLobbyGameServer( CSteamID steamIDLobby, uint32 unGameServerIP, uint16 unGameServerPort, CSteamID steamIDGameServer ) = 0;
+	virtual bool GetLobbyGameServer( CSteamID steamIDLobby, uint32 *punGameServerIP, uint16 *punGameServerPort, CSteamID *psteamIDGameServer ) = 0;
+
+	virtual bool SetLobbyMemberLimit( CSteamID steamIDLobby, int cMaxMembers ) = 0;
+	virtual int GetLobbyMemberLimit( CSteamID steamIDLobby ) = 0;
+
+	virtual void SetLobbyVoiceEnabled( CSteamID steamIDLobby, bool bVoiceEnabled ) = 0;
+	virtual bool RequestFriendsLobbies() = 0;
+
+	virtual bool SetLobbyType( CSteamID steamIDLobby, ELobbyType eLobbyType ) = 0;
+	virtual bool SetLobbyJoinable( CSteamID steamIDLobby, bool bLobbyJoinable ) = 0;
+	virtual CSteamID GetLobbyOwner( CSteamID steamIDLobby ) = 0;
+	virtual bool SetLobbyOwner( CSteamID steamIDLobby, CSteamID steamIDNewOwner ) = 0;
+
+	virtual int GetGMSServerCount() = 0;
+	virtual bool GetGMSServerAddress( int iServer, uint32 *unServerIP, uint16 *usServerPort ) = 0;
 };
 
 
