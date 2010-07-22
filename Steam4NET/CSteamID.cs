88b5324555b61e100e0c8464cf587a1e3b39c3e1
Ryan Stecker <voidedweasel@gmail.com>
2010-07-22 19:26:07 +0000

    Beefed up Steam4NET's CSteamID.
    --HG--
    extra : convert_revision : svn%3Af45044ad-9681-42ff-a337-a86ec09c9930%40242

--- Steam4NET/CSteamID.cs
+++ Steam4NET/CSteamID.cs
@@ -6,7 +6,10 @@ namespace Steam4NET
     {
         private UInt64 data;
 
-        public BitVector64(UInt64 value)
+        public BitVector64()
+        {
+        }
+        public BitVector64( UInt64 value )
         {
             data = value;
         }
@@ -14,17 +17,18 @@ namespace Steam4NET
         public UInt64 Data
         {
             get { return data; }
+            set { data = value; }
         }
 
-        public UInt64 this[uint bitoffset, UInt64 valuemask]
+        public UInt64 this[ uint bitoffset, UInt64 valuemask ]
         {
             get
             {
-                return (data >> (ushort)bitoffset) & valuemask;
+                return ( data >> ( ushort )bitoffset ) & valuemask;
             }
             set
             {
-                data = (data & ~(valuemask << (ushort)bitoffset) ) | ( (value & valuemask) << (ushort)bitoffset);
+                data = ( data & ~( valuemask << ( ushort )bitoffset ) ) | ( ( value & valuemask ) << ( ushort )bitoffset );
             }
         }
     }
@@ -33,20 +37,127 @@ namespace Steam4NET
     {
         private BitVector64 steamid;
 
-        public CSteamID(UInt64 id)
+        public CSteamID()
+            : this( 0 )
         {
-            steamid = new BitVector64(id);
+        }
+
+        public CSteamID( UInt32 unAccountID, EUniverse eUniverse, EAccountType eAccountType )
+            : this()
+        {
+            Set( unAccountID, eUniverse, eAccountType );
+        }
+
+        public CSteamID( UInt32 unAccountID, UInt32 unInstance, EUniverse eUniverse, EAccountType eAccountType )
+            : this()
+        {
+            InstancedSet( unAccountID, unInstance, eUniverse, eAccountType );
+        }
+
+        public CSteamID( UInt64 id )
+        {
+            this.steamid = new BitVector64( id );
+        }
+
+        public void Set( UInt32 unAccountID, EUniverse eUniverse, EAccountType eAccountType )
+        {
+            this.AccountID = unAccountID;
+            this.AccountUniverse = eUniverse;
+            this.AccountType = eAccountType;
+
+            if ( eAccountType == EAccountType.k_EAccountTypeClan )
+            {
+                this.AccountInstance = 0;
+            }
+            else
+            {
+                this.AccountInstance = 1;
+            }
+        }
+
+        public void InstancedSet( UInt32 unAccountID, UInt32 unInstance, EUniverse eUniverse, EAccountType eAccountType )
+        {
+            this.AccountID = unAccountID;
+            this.AccountUniverse = eUniverse;
+            this.AccountType = eAccountType;
+            this.AccountInstance = unInstance;
+        }
+
+        public void SetFromUint64( UInt64 ulSteamID )
+        {
+            this.steamid.Data = ulSteamID;
+        }
+
+        public UInt64 ConvertToUint64()
+        {
+            return this.steamid.Data;
+        }
+
+        public bool BBlankAnonAccount()
+        {
+            return this.AccountID == 0 && BAnonAccount() && this.AccountInstance == 0;
+        }
+        public bool BGameServerAccount()
+        {
+            return this.AccountType == EAccountType.k_EAccountTypeGameServer || this.AccountType == EAccountType.k_EAccountTypeAnonGameServer;
+        }
+        public bool BContentServerAccount()
+        {
+            return this.AccountType == EAccountType.k_EAccountTypeContentServer;
+        }
+        public bool BClanAccount()
+        {
+            return this.AccountType == EAccountType.k_EAccountTypeClan;
+        }
+        public bool BChatAccount()
+        {
+            return this.AccountType == EAccountType.k_EAccountTypeChat;
+        }
+        public bool IsLobby()
+        {
+            return ( this.AccountType == EAccountType.k_EAccountTypeChat ) && ( ( this.AccountInstance & ( 0x000FFFFF + 1 ) >> 2 ) != 0 );
+        }
+        public bool BAnonAccount()
+        {
+            return this.AccountType == EAccountType.k_EAccountTypeAnonUser || this.AccountType == EAccountType.k_EAccountTypeAnonGameServer;
+        }
+        public bool BAnonUserAccount()
+        {
+            return this.AccountType == EAccountType.k_EAccountTypeAnonUser;
+        }
+
+        public bool IsValid()
+        {
+            if ( this.AccountType <= EAccountType.k_EAccountTypeInvalid || this.AccountType >= EAccountType.k_EAccountTypeMax )
+                return false;
+
+            if ( this.AccountUniverse <= EUniverse.k_EUniverseInvalid || this.AccountUniverse >= EUniverse.k_EUniverseMax )
+                return false;
+
+            if ( this.AccountType == EAccountType.k_EAccountTypeIndividual )
+            {
+                if ( this.AccountID == 0 || this.AccountInstance != 1 )
+                    return false;
+            }
+
+            if ( this.AccountType == EAccountType.k_EAccountTypeClan )
+            {
+                if ( this.AccountID == 0 || this.AccountInstance != 0 )
+                    return false;
+            }
+
+            return true;
         }
 
         public UInt32 AccountID
         {
             get
             {
-                return (UInt32)steamid[0, 0xFFFFFFFF];
+                return ( UInt32 )steamid[ 0, 0xFFFFFFFF ];
             }
             set
             {
-                steamid[0, 0xFFFFFFFF] = value;
+                steamid[ 0, 0xFFFFFFFF ] = value;
             }
         }
 
@@ -54,11 +165,11 @@ namespace Steam4NET
         {
             get
             {
-                return (UInt32)steamid[32, 0xFFFFF];
+                return ( UInt32 )steamid[ 32, 0xFFFFF ];
             }
             set
             {
-                steamid[32, 0xFFFFF] = (UInt64)value;
+                steamid[ 32, 0xFFFFF ] = ( UInt64 )value;
             }
         }
 
@@ -66,11 +177,11 @@ namespace Steam4NET
         {
             get
             {
-                return (EAccountType)steamid[52, 0xF];
+                return ( EAccountType )steamid[ 52, 0xF ];
             }
             set
             {
-                steamid[52, 0xF] = (UInt64)value;
+                steamid[ 52, 0xF ] = ( UInt64 )value;
             }
         }
 
@@ -78,26 +189,26 @@ namespace Steam4NET
         {
             get
             {
-                return (EUniverse)steamid[56, 0xFF];
+                return ( EUniverse )steamid[ 56, 0xFF ];
             }
             set
             {
-                steamid[56, 0xFF] = (UInt64)value;
+                steamid[ 56, 0xFF ] = ( UInt64 )value;
             }
         }
 
         public string Render()
         {
-            switch (AccountType)
+            switch ( AccountType )
             {
                 case EAccountType.k_EAccountTypeInvalid:
                 case EAccountType.k_EAccountTypeIndividual:
-                    if(AccountUniverse <= EUniverse.k_EUniversePublic)
-                        return String.Format("STEAM_0:{0}:{1}", AccountID & 1, AccountID >> 1);
+                    if ( AccountUniverse <= EUniverse.k_EUniversePublic )
+                        return String.Format( "STEAM_0:{0}:{1}", AccountID & 1, AccountID >> 1 );
                     else
-                        return String.Format("STEAM_{2}:{0}:{1}", AccountID & 1, AccountID >> 1, (int)AccountUniverse);
+                        return String.Format( "STEAM_{2}:{0}:{1}", AccountID & 1, AccountID >> 1, ( int )AccountUniverse );
                 default:
-                    return Convert.ToString(this);
+                    return Convert.ToString( this );
             }
         }
 
@@ -113,43 +224,43 @@ namespace Steam4NET
 
         public static implicit operator CSteamID( UInt64 id )
         {
-            return new CSteamID(id);
+            return new CSteamID( id );
         }
 
-        public override bool Equals(System.Object obj)
+        public override bool Equals( System.Object obj )
         {
-            if (obj == null)
+            if ( obj == null )
                 return false;
 
             CSteamID sid = obj as CSteamID;
-            if ((System.Object)sid == null)
+            if ( ( System.Object )sid == null )
                 return false;
 
             return steamid.Data == sid.steamid.Data;
         }
 
-        public bool Equals(CSteamID sid)
+        public bool Equals( CSteamID sid )
         {
-            if ((object)sid == null)
+            if ( ( object )sid == null )
                 return false;
 
             return steamid.Data == sid.steamid.Data;
         }
 
-        public static bool operator ==(CSteamID a, CSteamID b)
+        public static bool operator ==( CSteamID a, CSteamID b )
         {
-            if (System.Object.ReferenceEquals(a, b))
+            if ( System.Object.ReferenceEquals( a, b ) )
                 return true;
 
-            if (((object)a == null) || ((object)b == null))
+            if ( ( ( object )a == null ) || ( ( object )b == null ) )
                 return false;
 
             return a.steamid.Data == b.steamid.Data;
         }
 
-        public static bool operator !=(CSteamID a, CSteamID b)
+        public static bool operator !=( CSteamID a, CSteamID b )
         {
-            return !(a == b);
+            return !( a == b );
         }
 
         public override int GetHashCode()
