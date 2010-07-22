5cbc515ac4f97d77c8afb565dad224a3ca5bdd32
Asher Baker <asherkin@limetech.org>
2010-07-22 03:21:46 +0000

    Patch by ShaRose for Steam4Test (closes #34)
    --HG--
    extra : convert_revision : svn%3Af45044ad-9681-42ff-a337-a86ec09c9930%40240

--- Steam4NET/Steam4Test/Program.cs
+++ Steam4NET/Steam4Test/Program.cs
@@ -29,10 +29,10 @@ namespace Steam4Test
             ISteamUtils004 utils = Steamworks.CastInterface<ISteamUtils004>(steamclient.GetISteamUtils(pipe, "SteamUtils004"));
             ISteamFriends005 friends = Steamworks.CastInterface<ISteamFriends005>(steamclient.GetISteamFriends(user, pipe, "SteamFriends005"));
 
-            int num = friends.GetFriendCount((int)k_EFriendFlags.k_EFriendFlagAll);
+             int num = friends.GetFriendCount(EFriendFlags.k_EFriendFlagAll);
             for (int i = 0; i < num; i++)
             {
-                CSteamID id = friends.GetFriendByIndex(i, (int)k_EFriendFlags.k_EFriendFlagAll);
+                CSteamID id = friends.GetFriendByIndex(i, (int)EFriendFlags.k_EFriendFlagAll);
                 string name = friends.GetFriendPersonaName(id);
 
                 Debug.WriteLine(name);
