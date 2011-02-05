2c1a14f2369785050be0060057ce42b20047c590
didrole <didrole@gmail.com>
2011-02-05 17:38:49 +0000

    Fixed ISteamApps004::GetPurchaseTime definition
    --HG--
    extra : convert_revision : svn%3Af45044ad-9681-42ff-a337-a86ec09c9930%40319

--- Open Steamworks/ISteamApps004.h
+++ Open Steamworks/ISteamApps004.h
@@ -42,8 +42,8 @@ public:
 	// Takes AppID of DLC and checks if the user owns the DLC & if the DLC is installed
 	virtual bool IsDlcInstalled( AppId_t appID ) = 0;
 
-	// I'm not sure about these functions names
-	virtual int GetPurchaseTime() = 0;
+	// I'm not sure about theses functions names
+	virtual uint32 GetPurchaseTime( AppId_t appID ) = 0;
 	virtual bool IsFreeWeekend() = 0;
 };
 
