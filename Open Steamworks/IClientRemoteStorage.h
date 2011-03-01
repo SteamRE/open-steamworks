ea5f85d76f9562689f38003bf0c5b734142fb287
Ryan Stecker <voidedweasel@gmail.com>
2011-03-01 19:00:08 +0000

    Small fix so GCCXML doesn't explode.
    --HG--
    extra : convert_revision : svn%3Af45044ad-9681-42ff-a337-a86ec09c9930%40339

--- Open Steamworks/IClientRemoteStorage.h
+++ Open Steamworks/IClientRemoteStorage.h
@@ -67,7 +67,7 @@ public:
 	virtual uint32 GetNumGamesWithLocalScreenshots() = 0;
 	virtual CGameID GetGameWithLocalScreenshots( uint32 uIndex ) = 0;
 	virtual uint32 GetLocalScreenshotCount( CGameID GameID ) = 0;
-	virtual bool GetLocalScreenshot( CGameID GameID, uint32 uIndex, char* pchFilename, uint32 cubFilename, uint32* puWidth, uint32* puHeight, uint32* puTimestamp, EScreenshotPrivacy* pePrivacy, uint64* pulUnk /*ID ?*/, char* pchCaption, uint32 cubCaption ) = 0;
+	virtual bool GetLocalScreenshot( CGameID GameID, uint32 uIndex, char* pchFilename, uint32 cubFilename, uint32* puWidth, uint32* puHeight, uint32* puTimestamp, EScreenshotPrivacy* pePrivacy, uint64* pulUnk, char* pchCaption, uint32 cubCaption ) = 0;
 	virtual bool SetLocalScreenshotCaption( CGameID GameID, const char * szFilename, const char * szCaption ) = 0;
 	virtual bool SetLocalScreenshotPrivacy( CGameID GameID, const char * szFilename, EScreenshotPrivacy ePrivacy ) = 0;
 	virtual bool ScreenshotStartBatch( CGameID GameID ) = 0;
