a61120054e764a2983cd509f0924ba5c8f489d16
Netshroud <netshroud@gmail.com>
2014-03-22 23:22:34 +1100

    Update IClientApps

--- Open Steamworks/IClientApps.h
+++ Open Steamworks/IClientApps.h
@@ -32,6 +32,8 @@ public:
 	// If you expect it to exists wait for the AppDataChanged_t after the first failure and ask again
 	virtual int32 GetAppData( AppId_t unAppID, const char *pchKey, char *pchValue, int32 cchValueMax ) = 0;
 
+	virtual bool SetLocalAppConfig( AppId_t unAppID, uint8 *pchBuffer, int32 cbBuffer ) = 0;
+
 	virtual AppId_t GetInternalAppIDFromGameID( CGameID nGameID ) = 0;
 
 	virtual int32 GetAllOwnedMultiplayerApps( uint32 *punAppIDs, int32 cAppIDsMax ) = 0;
@@ -40,12 +42,9 @@ public:
 	virtual bool RequestAppInfoUpdate( const AppId_t *pAppIDs, int32 nNumAppIDs ) = 0;
 
 	virtual void NotifyAppEventTriggered( AppId_t unAppID, EAppEvent eAppEvent ) = 0;
-	virtual void NotifyDlcInstalled( AppId_t unAppID ) = 0;
 
 	virtual int32 GetDLCCount( AppId_t unAppID ) = 0;
 	virtual bool BGetDLCDataByIndex( AppId_t unAppID, int32 iDLC, AppId_t* pDlcAppID, bool *pbAvailable, char *pchName, int32 cchNameBufferSize ) = 0;
-		
-	virtual bool BIsDlcInstalled( AppId_t unDlcAppID, AppId_t unGameAppID ) = 0;
 };
 
 #endif // ICLIENTAPPS_H
